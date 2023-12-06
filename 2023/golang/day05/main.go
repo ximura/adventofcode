d6e385186a27e03da1a17b598993c528b7151fca
Oleh Rukin <3680312+ximura@users.noreply.github.com>
2023-12-06 17:09:48 +0200

    day05 part1

--- 2023/golang/day05/main.go
+++ 2023/golang/day05/main.go
@@ -10,9 +10,40 @@ import (
 )
 
 var inputs = "../inputs/day05.txt"
+var seedKey = "seed"
+
+type pair struct {
+	source string
+	target string
+}
+
+var mapper = map[string]pair{
+	"seed-to-soil map:":            {source: seedKey, target: "soil"},
+	"soil-to-fertilizer map:":      {source: "soil", target: "fertilizer"},
+	"fertilizer-to-water map:":     {source: "fertilizer", target: "water"},
+	"water-to-light map:":          {source: "water", target: "light"},
+	"light-to-temperature map:":    {source: "light", target: "temperature"},
+	"temperature-to-humidity map:": {source: "temperature", target: "humidity"},
+	"humidity-to-location map:":    {source: "humidity", target: "location"},
+}
 
 type seed struct {
 	values map[string]int
+}
+
+func (s seed) fillWithDefault(src, dst string) {
+	_, ok := s.values[dst]
+	if !ok {
+		s.values[dst] = s.values[src]
+	}
+}
+
+type seeds map[int]*seed
+
+func (s seeds) fillWithDefault(src, dst string) {
+	for i := range s {
+		s[i].fillWithDefault(src, dst)
+	}
 }
 
 func main() {
@@ -22,60 +53,68 @@ func main() {
 	}
 	defer file.Close()
 
-	seeds := make(map[int]seed)
-
+	s := make(seeds)
+	key := pair{}
 	part1 := 0
 	part2 := 0
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
 		line := scanner.Text()
-		parse(seeds, line)
+		newKey, ok := mapper[line]
+		if ok {
+			key = newKey
+			s.fillWithDefault(key.source, key.target)
+			continue
+		}
+		parse(s, key, line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+	minLocation := 9223372036854775807
+	for i := range s {
+		l := s[i].values["location"]
+		if l < minLocation {
+			minLocation = l
+			part1 = l
+		}
+	}
+
 	log.Printf("Part1 : %d\n", part1)
 	log.Printf("Part2 : %d\n", part2)
 }
 
-func parse(s map[int]seed, str string) {
+func parse(s seeds, p pair, str string) {
 	if strings.HasPrefix(str, "seeds:") {
 		parseSeeds(s, str)
 		return
 	}
 
-	m := ""
-	if strings.HasPrefix(str, "seed-to-soil map:") {
-		m = "soil"
-	}
-	if strings.HasPrefix(str, "soil-to-fertilizer map:") {
-		m = "fertilizer"
-	}
-
 	re := regexp.MustCompile(`(\d+)`)
-	matches := re.FindAllStringSubmatch(parts[0], -1)
-	for _, match := range matches {
-		score, _ := strconv.Atoi(match[1])
-		card.victory = append(card.victory, score)
+	matches := re.FindAllStringSubmatch(str, -1)
+	if len(matches) < 3 {
+		return
 	}
+	desStart, _ := strconv.Atoi(matches[0][1])
+	sourceStart, _ := strconv.Atoi(matches[1][1])
+	count, _ := strconv.Atoi(matches[2][1])
 
-	matches = re.FindAllStringSubmatch(parts[1], -1)
-	for _, match := range matches {
-		score, _ := strconv.Atoi(match[1])
-		if card.member(score) {
-			card.increaseScore()
+	for i := range s {
+		v := s[i].values[p.source]
+		if v >= sourceStart && v < sourceStart+count {
+			s[i].values[p.target] = desStart + (v - sourceStart)
 		}
 	}
 }
 
-func parseSeeds(s map[int]seed, str string) {
+func parseSeeds(s seeds, str string) {
 	re := regexp.MustCompile(`(\d+)`)
 	matches := re.FindAllStringSubmatch(str, -1)
 	for _, match := range matches {
 		sd, _ := strconv.Atoi(match[1])
-		s[sd] = seed{}
+		s[sd] = &seed{values: map[string]int{seedKey: sd, "soil": sd}}
 	}
 }

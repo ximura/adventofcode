993bfa6d5fde08d81e939eb9aa7724c70a2c6b09
Oleh Rukin <3680312+ximura@users.noreply.github.com>
2024-12-03 22:28:18 +0200

    part 2

--- 2024/golang/day02/main.go
+++ 2024/golang/day02/main.go
@@ -25,6 +25,7 @@ func NewReport() Report {
 	return Report{
 		data:      make([]int, 0, 10),
 		direction: DirectionNone,
+		unsafePos: -1,
 		safe:      true,
 	}
 }
@@ -32,16 +33,18 @@ func NewReport() Report {
 type Report struct {
 	data      []int
 	direction DirectionType
+	unsafePos int
 	safe      bool
 }
 
-func (r *Report) insert(v, index int) {
-	if index == -1 {
+func (r *Report) insert(v int) {
+	l := len(r.data)
+	if l == 0 {
 		r.data = append(r.data, v)
 		return
 	}
 
-	dif := v - r.data[index]
+	dif := v - r.data[l-1]
 	newDirection := DirectionNone
 	if dif > 0 {
 		newDirection = DirectionUp
@@ -58,12 +61,81 @@ func (r *Report) insert(v, index int) {
 		newDirection != DirectionNone &&
 		r.direction == newDirection &&
 		abs(dif) < 4
+
+	if !r.safe && r.unsafePos == -1 {
+		r.unsafePos = l - 1
+	}
+
 	r.data = append(r.data, v)
+}
+
+func (r *Report) isSafe(data []int, skip int) bool {
+	l := len(data) - 1
+
+	d := make([]int, 0, l)
+	d = append(d, data[:skip]...)
+	d = append(d, data[skip+1:]...)
+	direction := DirectionNone
+	i := 1
+	for {
+		dif := d[i] - d[i-1]
+		newDirection := DirectionNone
+		if dif > 0 {
+			newDirection = DirectionUp
+		}
+
+		if dif < 0 {
+			newDirection = DirectionDown
+		}
+		if direction == DirectionNone {
+			direction = newDirection
+		}
+
+		safe := newDirection != DirectionNone &&
+			direction == newDirection &&
+			abs(dif) < 4
+
+		if !safe {
+			return false
+		}
+
+		i++
+		if i >= l {
+			break
+		}
+	}
+
+	return true
+}
+
+func (r *Report) applyDamper() bool {
+	return r.safe ||
+		r.isSafe(r.data, 0) ||
+		r.isSafe(r.data, r.unsafePos) ||
+		r.isSafe(r.data, r.unsafePos+1) ||
+		r.isSafe(r.data, r.unsafePos+2)
 }
 
 type Levels struct {
 	reports []Report
 	count   int
+}
+
+func (l *Levels) applyDamper() int {
+	count := 0
+	for _, r := range l.reports {
+		if r.applyDamper() {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (l *Levels) print() {
+	for _, r := range l.reports {
+		fmt.Printf("%+v\n", r)
+	}
 }
 
 func main() {
@@ -80,7 +152,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", lvl.count)
-	fmt.Printf("Part 2: %d\n", 0)
+	fmt.Printf("Part 2: %d\n", lvl.applyDamper())
 	fmt.Printf("Time: %.2fms\n", float64(time.Since(timeStart).Microseconds())/1000)
 }
 
@@ -99,9 +171,9 @@ func parseFile(r io.Reader) (Levels, error) {
 		}
 
 		r := NewReport()
-		for i, b := range p {
+		for _, b := range p {
 			data, _ := strconv.Atoi(string(b))
-			r.insert(data, i-1)
+			r.insert(data)
 		}
 
 		lvl.reports = append(lvl.reports, r)

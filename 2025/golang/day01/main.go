5469473ce3f60bc2f7ed0fd60268706b49472c0d
Oleh Rukin <3680312+ximura@users.noreply.github.com>
2025-12-02 07:52:06 +0200

    upd

--- 2025/golang/day01/main.go
+++ 2025/golang/day01/main.go
@@ -33,8 +33,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Part 1: %d\n", part1(d))
-	fmt.Printf("Part 2: %d\n", part2(d))
+	p1, p2 := calcualte(d)
+	fmt.Printf("Part 1: %d\n", p1)
+	fmt.Printf("Part 2: %d\n", p2)
 	fmt.Printf("Time: %.2fms\n", float64(time.Since(timeStart).Microseconds())/1000)
 }
 
@@ -55,6 +56,51 @@ func parseFile(r io.Reader) (Data, error) {
 	}
 
 	return d, nil
+}
+
+func calcualte(d Data) (int, int) {
+	start := 50
+	p1, p2 := 0, 0
+
+	//fmt.Println("The dial starts by pointing at 50.")
+	for i := range d {
+		step := d[i]
+		amount := step.amount % 100
+		fullCircle := step.amount / 100
+		startWithZero := start == 0
+		passZero := fullCircle
+
+		if step.direction {
+			start += amount
+		} else {
+			start -= amount
+		}
+
+		if start == 0 || start == 100 {
+			p1++
+			passZero++
+			start = 0
+		}
+
+		if start > 100 {
+			start -= 100
+			passZero++
+		} else if start < 0 {
+			start = 100 + start
+			if !startWithZero {
+				passZero++
+			}
+		}
+
+		p2 += passZero
+
+		// fmt.Printf("The dial is rotated %s to point at %d.\n", step.original, start)
+		// if passZero > 0 {
+		// 	fmt.Printf("during this rotation, it points at 0: %d.\n", passZero)
+		// }
+	}
+
+	return p1, p2
 }
 
 func part1(d Data) int {
